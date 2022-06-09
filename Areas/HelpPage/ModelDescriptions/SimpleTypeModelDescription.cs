95a0e59f5c8abf8ca1081e8a8116093f8ce7300b
Kachan_Mykhailo <mykhailo.kachan.pz.2019@edu.lpnu.ua>
2022-06-09 13:07:40 +0300

    Initial code commit. Here's the default API for Identity.

---
+++ Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace BlackMarket_API.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BlackMarket_API.WebApiApplication" Language="C#" %>
